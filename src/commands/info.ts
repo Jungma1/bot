f3669d8ad091fb69a111b7fb1f3e248c8cc7d33c
Jungma1 <mbjun3630@naver.com>
2022-02-21 16:27:28 +0900

    Update info command message style

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -1,22 +1,52 @@
 import { Command } from '../interfaces/Command';
-import { SlashCommandBuilder } from '@discordjs/builders';
+import { SlashCommandBuilder, hyperlink } from '@discordjs/builders';
 import { MessageEmbedOptions } from 'discord.js';
+import blank from '../lib/blank';
 
 export const info: Command = {
   name: '정보',
+  /**
+   * Slash command data
+   */
   data: new SlashCommandBuilder()
     .setName('정보')
     .setDescription('NARE 봇의 정보를 표시합니다.'),
+  /**
+   * Slash command run
+   */
   run: async (interaction) => {
+    const githubLink = hyperlink(
+      'NARE_BOT_GITHUB',
+      'https://github.com/jungma1/nare-bot'
+    );
     const infoEmbedOptions: MessageEmbedOptions = {
       color: 'AQUA',
       author: {
         name: interaction.client.user?.username,
         iconURL: interaction.client.user?.displayAvatarURL(),
       },
-      description: `'중마' 님이 개발한 서버 관리 봇 입니다.`,
+      description: `SpaceTime 전용 서버 관리 봇 입니다.`,
+      fields: [
+        { name: '\u200B', value: '\u200B' },
+        {
+          name: '🌐│ 핑',
+          value: `ㄴ ${interaction.client.ws.ping.toString()} \t`,
+        },
+        { name: '🅿️│ 개발 언어', value: 'ㄴ Typescript' },
+        { name: '🛠│ 사용된 라이브러리', value: 'ㄴ discord.js (13.3.1)' },
+        {
+          name: `<:github:945216252723490816>│ 깃허브 링크`,
+          value: `ㄴ ${githubLink}`,
+        },
+        { name: '\u200B', value: '\u200B' },
+      ],
+      timestamp: new Date(),
+      footer: {
+        text: interaction.user.username,
+        iconURL: interaction.user.displayAvatarURL(),
+      },
     };
 
-    await interaction.reply({ embeds: [infoEmbedOptions] });
+    await interaction.reply({ embeds: [infoEmbedOptions], ephemeral: true });
   },
 };
---
+++ src/lib/blank.ts
@@ -0,0 +1,5 @@
+export default function blank(num: number) {
+  const blankEmoji = '<:Blank:850603065760284722>';
+
+  return blankEmoji.repeat(num);
+}
