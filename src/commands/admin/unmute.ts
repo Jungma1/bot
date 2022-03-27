a2587c5274f64b2a3fa9ab5bc67729b90f486596
Jungma1 <mbjun3630@naver.com>
2022-03-27 15:45:15 +0900

    Feat add unmute command

--- src/commands/admin/unmute.ts
+++ src/commands/admin/unmute.ts
@@ -3,16 +3,16 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 import { GuildMember, MessageEmbed } from 'discord.js';
 import config from '../../config';
 
-export const mute: Command = {
-  name: '뮤트',
+export const unmute: Command = {
+  name: '뮤트해제',
   permission: true,
   data: new SlashCommandBuilder()
-    .setName('뮤트')
-    .setDescription('사용자를 뮤트 시킵니다.')
+    .setName('뮤트해제')
+    .setDescription('사용자의 뮤트를 해제시킵니다.')
     .addUserOption((option) =>
       option
         .setName('사용자')
-        .setDescription('뮤트 시킬 사용자를 선택합니다.')
+        .setDescription('뮤트를 해제시킬 사용자를 선택합니다.')
         .setRequired(true)
     ),
   run: async (interaction) => {
@@ -31,18 +31,18 @@ export const mute: Command = {
       return interaction.reply({ embeds: [sendEmbed], ephemeral: true });
     }
 
-    if (target.roles.cache.has(ROLE_MUTED_ID)) {
+    if (!target.roles.cache.has(ROLE_MUTED_ID)) {
       sendEmbed
         .setColor('DARK_RED')
-        .setDescription(`${target} / 이미 뮤트 상태입니다.`);
+        .setDescription(`${target} / 뮤트 상태가 아닙니다.`);
 
       return interaction.reply({ embeds: [sendEmbed], ephemeral: true });
     }
 
-    await target.roles.add(ROLE_MUTED_ID);
+    await target.roles.remove(ROLE_MUTED_ID);
     sendEmbed
       .setColor('DARK_GREEN')
-      .setDescription(`${target} / 뮤트를 시작합니다.`);
+      .setDescription(`${target} / 뮤트를 해제하였습니다.`);
 
     return interaction.reply({ embeds: [sendEmbed] });
   },
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,6 @@
 import { Command } from '../interfaces/Command';
 import { mute } from './admin/mute';
+import { unmute } from './admin/unmute';
 import { info } from './info';
 
-export const CommandList: Command[] = [info, mute];
+export const CommandList: Command[] = [info, mute, unmute];
