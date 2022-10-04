cb9b2c4fafe9ab9ee8b7ba570a8ae88609a1813f
Jungma <mbjun3630@naver.com>
2022-10-04 12:50:01 +0900

    refactor: reply riot embed

--- src/commands/search/lol.ts
+++ src/commands/search/lol.ts
@@ -1,16 +1,10 @@
-import {
-  CommandInteractionOptionResolver,
-  EmbedBuilder,
-  SlashCommandBuilder,
-} from 'discord.js';
+import { CommandInteractionOptionResolver, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
 import { Command } from '../../interface/Command';
 import {
   findSummonerDataByName,
   findSummonerLeagueDataById,
   LeagueGamemodeType,
-  RANKED_FLEX_SR,
   RANKED_SOLO,
-  RANKED_TFT_DOUBLE_UP,
   UNRANKED,
 } from '../../lib/api/riotClient';
 import { replyRiotEmbed } from '../../lib/utils/replyRiotEmbed';
@@ -21,13 +15,10 @@ export const lol: Command = {
   data: new SlashCommandBuilder()
     .setName('롤전적')
     .setDescription('간단한 명령어로 롤 전적을 확인할 수 있어요!')
-    .addStringOption((option) =>
-      option
-        .setName('username')
-        .setDescription('찾으려고 하는 유저의 이름')
-        .setRequired(true)
+    .addStringOption(option =>
+      option.setName('username').setDescription('찾으려고 하는 유저의 이름').setRequired(true)
     ),
-  run: async (interaction) => {
+  run: async interaction => {
     const options = interaction.options as CommandInteractionOptionResolver;
     const username = options.getString('username', true);
     const summonerData = await findSummonerDataByName(username);
@@ -42,9 +33,7 @@ export const lol: Command = {
       });
     }
 
-    const summonerLeagueData = await findSummonerLeagueDataById(
-      summonerData.id
-    );
+    const summonerLeagueData = await findSummonerLeagueDataById(summonerData.id);
 
     if (summonerLeagueData.length === 0) {
       replyRiotEmbed(UNRANKED, interaction, summonerData, summonerLeagueData);
@@ -53,31 +42,6 @@ export const lol: Command = {
     // const gamemode = summonerLeagueData.flatMap((league) => league.queueType);
     const choiceMode: LeagueGamemodeType = RANKED_SOLO;
 
-    if (choiceMode === RANKED_SOLO) {
-      return replyRiotEmbed(
-        RANKED_SOLO,
-        interaction,
-        summonerData,
-        summonerLeagueData
-      );
-    }
-
-    if (choiceMode === RANKED_FLEX_SR) {
-      return replyRiotEmbed(
-        RANKED_FLEX_SR,
-        interaction,
-        summonerData,
-        summonerLeagueData
-      );
-    }
-
-    if (choiceMode === RANKED_TFT_DOUBLE_UP) {
-      return replyRiotEmbed(
-        RANKED_TFT_DOUBLE_UP,
-        interaction,
-        summonerData,
-        summonerLeagueData
-      );
-    }
+    return replyRiotEmbed(choiceMode, interaction, summonerData, summonerLeagueData);
   },
 };
